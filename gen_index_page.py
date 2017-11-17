69a709ec6e692913ae5c8916929286bb78f65f51
Milt Epstein <mepstein@illinois.edu>
2017-11-17 14:48:20 -0600

    Added saving repos info to a file (option -s) Added saving output as html rather than markdown (option -H) Cleaned up some code/output/comment

--- gen_index_page.py
+++ gen_index_page.py
@@ -7,8 +7,7 @@
 
 import argparse
 import json
-#import markdown
-#html = markdown.markdown(your_text_string)
+import markdown
 import pprint
 import sys
 import urllib.request
@@ -25,6 +24,7 @@ DEFAULT_ORG = 'KnowEnG'
 #---
 #DEFAULT_OUTPUT_FILE = 'index.md'
 DEFAULT_OUTPUT_FILE = 'out.md'
+DEFAULT_REPOS_INFO_FILE = 'repos.info.json'
 
 GIT_ORG_URL = 'https://github.com/:org:'
 GIT_REPO_URL = 'https://github.com/:org:/:repo:'
@@ -41,6 +41,8 @@ def parse_args():
     parser.add_argument('-rmf', '--repos_metadata_file', default=DEFAULT_REPOS_METADATA_FILE)
     parser.add_argument('-o', '--org', default=DEFAULT_ORG)
     parser.add_argument('-of', '--output_file', default=DEFAULT_OUTPUT_FILE)
+    parser.add_argument('-H', '--to_html', action='store_true')
+    parser.add_argument('-s', '--save_repos_info', action='store_true')
 
     args = parser.parse_args()
 
@@ -78,15 +80,17 @@ def get_repos_info(args):
         #with urllib.request.urlopen(url) as f:
         with urllib.request.urlopen(req) as f:
             org_info_str = f.read().decode('utf-8')
-    #print(org_info_str)
 
+    # This will be a dict
     org_info = json.loads(org_info_str)
-    #print(org_info)
-    #print(type(org_info))
+    # This will be a list (of dicts)
     items = org_info["items"]
-    #print(items)
-    #print(type(items))
-    #print(len(items))
+
+    if args.save_repos_info:
+        outf = open(DEFAULT_REPOS_INFO_FILE, 'w')
+        # A little odd, calling json.dumps() again, but it works to pretty print it
+        print(json.dumps(org_info, indent=2), file=outf)
+        outf.close()
 
     repos_info = {}
 
@@ -95,6 +99,7 @@ def get_repos_info(args):
         print("  Found repo '%s'" % (name))
         repos_info[name] = item
 
+    print("Found %d repos in the repos info" % (len(items)))
     print()
 
     return repos_info
@@ -103,29 +108,36 @@ def get_repos_info(args):
 def main():
     args = parse_args()
 
+    # Get the repos info
     repos_info = get_repos_info(args)
 
+    # Get the repos metadata
     print("Getting repos metadata from file '%s' ..." % (args.repos_metadata_file))
     print()
     with open(args.repos_metadata_file, 'r') as f:
-        repo_metadata = yaml.load(f)
+        repos_metadata = yaml.load(f)
 
     # This will be a list of dicts
     # Each of those dicts has one key (the category),
     # whose value is another list of dicts
     # Likewise each of those dicts has one key (the repo),
     # and its value is another list of dicts (the properties of the repo)
-    # What about properties of categories?
-    #print(repo_metadata)
-    #print(type(repo_metadata))
+    # What about properties of categories?  Can't do it with this structure.
+    #print(repos_metadata)
+    #print(type(repos_metadata))
     #pp = pprint.PrettyPrinter(indent=4)
-    #pp.pprint(repo_metadata)
+    #pp.pprint(repos_metadata)
 
-    outf = open(args.output_file, 'w')
+    # Process the repos in the repos metadata
+    # The markdown string
+    markdown_str = ''
 
     repos_seen = {}
+    num_repos = 0
+    num_repos_public = 0
 
-    for category_data in repo_metadata:
+    print("Processing the categories and repos ...")
+    for category_data in repos_metadata:
         #print(category_data)
         # category is a dict with one key
         # That comma after the paren belongs there
@@ -133,20 +145,23 @@ def main():
         (category, repos_data), = category_data.items()
         print("Processing category '%s' ..." % (category))
         if repos_data:
-            print("## %s" % (category), file=outf)
+            markdown_str += "## %s\n" % (category)
         for repo_data in repos_data:
             #print(repo_data)
             (repo, repo_data_bare), = repo_data.items()
             print("  Processing repo '%s' ..." % (repo))
             repos_seen[repo] = 1
-            if "hide" in repo_data_bare and repo_data_bare["hide"]:
-                print("  hiding repo '%s'" % (repo))
-                continue
+            num_repos += 1
             if repo in repos_info:
+                num_repos_public += 1
+                if "hide" in repo_data_bare and repo_data_bare["hide"]:
+                    print("  hiding repo '%s'" % (repo))
+                    continue
                 # What repo_info to use?
                 # private, html_url, description
                 # topics, if present
                 repo_info = repos_info[repo]
+                #print(repo_info)
                 private = repo_info["private"]
                 html_url = repo_info["html_url"]
                 description = repo_info["description"]
@@ -154,22 +169,26 @@ def main():
                     topics = repo_info["topics"]
                 else:
                     topics = None
-                #print(repo_info)
                 if description is None:
-                    print(" - [%s](%s):" % (repo, html_url), file=outf)
+                    markdown_str += " - [%s](%s)\n" % (repo, html_url)
                 else:
                     description = description.rstrip()
-                    print(" - [%s](%s): %s" % (repo, html_url, description), file=outf)
+                    markdown_str += " - [%s](%s): %s\n" % (repo, html_url, description)
             else:
                 print("  repo '%s' is private" % (repo))
+    print("Found %d categories, %d repos (%d public) in the repos metadata" %
+          (len(repos_metadata), num_repos, num_repos_public))
     print()
 
+    #repos_not_seen = []
     #repos_not_seen = {}
 
     #for repo in repos_info:
     #    if repo not in repos_seen:
+    #        repos_not_seen.append(repo)
     #        repos_not_seen[repo] = 1
 
+    # Check if we didn't process any repos in the repos info
     repos_not_seen = [r for r in repos_info if not repos_seen[r]]
 
     if repos_not_seen:
@@ -177,6 +196,15 @@ def main():
         print(repos_not_seen)
     else:
         print("All of the repos in the repos info were processed.")
+
+    # Do the output
+    if args.to_html:
+        output = markdown.markdown(markdown_str)
+    else:
+        output = markdown_str
+    outf = open(args.output_file, 'w')
+    print(output, end='', file=outf)
+    outf.close()
 
 
 if __name__ == "__main__":
